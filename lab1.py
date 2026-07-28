8e1e858a69eacfe788b8ba93dce80c8fe3981e41
diyanarang <diyanarang.15@gmail.com>
2026-07-28 09:12:35 +0530

    assignment 4

--- lab1.py
+++ lab1.py
@@ -17,3 +17,29 @@ st2='two'
 st3='three'
 
 print(st1," ",a)
+
+#######
+#age=input('enter your age: ')
+#print(age)
+summ=0
+for k in range(1,11):
+    summ=summ+k
+print(summ)
+
+#table7
+for x7 in range(1,11):
+    print(x7*7)
+#table9
+for x9 in range(1,11):
+    print(x9*9)
+
+number=int(input("enter number: "))
+sum_n=0
+for n in range(1,number+1):
+    sum_n+=n
+print(sum_n)
+#table3
+print("table of 3: ")
+while a<=10:
+    print(3*a)
+    a+=1
